cdcc5075031924a16a94dcbb052e3e655282430f
David Masse <davidjmasse@hotmail.com>
2018-07-11 17:52:49 -0400

    Done.

--- queries.py
+++ queries.py
@@ -7,25 +7,35 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def return_apple():
-    pass
+    return session.query(Company).filter_by(company = 'Apple').first()
 
 def return_disneys_industry():
-    pass
+    disney = session.query(Company).filter_by(company = 'Walt Disney').first()
+    return disney.industry
 
 def return_list_of_company_objects_ordered_alphabetically_by_symbol():
-    pass
+    return session.query(Company).order_by(Company.symbol).all()
 
 def return_list_of_dicts_of_tech_company_names_and_their_EVs_ordered_by_EV_descending():
-    pass
+    comps = session.query(Company).filter_by(industry = "Technology").order_by(Company.enterprise_value.desc()).all()
+    dlist = []
+    for comp in comps:
+        dlist.append({'company': comp.company, 'EV': comp.enterprise_value})
+    return dlist
 
 def return_list_of_consumer_products_companies_with_EV_above_225():
-    pass
+    comps = session.query(Company).filter_by(industry = 'Consumer products').filter(Company.enterprise_value > 225).all()
+    dlist = []
+    for comp in comps:
+        dlist.append({'name': comp.company})
+    return dlist
 
 def return_conglomerates_and_pharmaceutical_companies():
-    pass
+    comps = session.query(Company).all()
+    return [comp.company for comp in comps if comp.industry == "Conglomerate" or comp.industry == "Pharmaceuticals"]
 
 def avg_EV_of_dow_companies():
-    pass
+    return session.query(func.avg(Company.enterprise_value)).first()
 
 def return_industry_and_its_total_EV():
-    pass
+    return session.query(Company.industry, func.sum(Company.enterprise_value)).group_by(Company.industry).order_by(Company.industry).all()
